940a0a2f77f051a63be3d9ef53207760160f6081
Prabakaran <prabakaran@lookman.in>
2021-09-16 16:36:30 +0530

    'booking-old-replaced'

--- model/booking.js
+++ model/booking.js
@@ -1,82 +1,126 @@
 import mongoose from 'mongoose'
-
 const bookingSchema = new mongoose.Schema({
-    add_on_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'addon'}],
-    amount_paid: Number,
-    appointment_booking_time: Date,
-    appointment_end_time: Date,
-    appointment_start_time: Date,
-    appointment_time_before_reschedule: [Date],
+  
+  appointment_booking_time: Date,
+  appointment_end_time: Date,
+  appointment_start_time: Date,
+  is_recurring: Boolean,
+  timezone: String,
     
-    cost: Number,
-    created_at: Date,
-    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'staff'},
-    created_from: String,
-    customer_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'customer'}],
-    customer_mood: String,
-    customer_status: String,
-    deleted: Boolean,
-    deleted_at: Date,
-    
-    duration_minutes: Number,
-    event_id: Event,
-    
-    is_recurring: Boolean,
-    is_rescheduled: Boolean,
-    is_review_given: Boolean,
-    location_link: String,
-    location_setting_id: { type: mongoose.Schema.Types.ObjectId, ref: 'locationsetting'},
-    note: String,
-    
-    site_id: { type: mongoose.Schema.Types.ObjectId, ref: 'site'},
-    slot: Number,
-    staff_id: { type: mongoose.Schema.Types.ObjectId, ref: 'staff'},
-    timezone: String,
-    transfered_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'staff'}],
-    type: String,
-    updated_at: Date,
-    workspace_id: { type: mongoose.Schema.Types.ObjectId, ref: 'workspace'}
-})
-
-const Booking = mongoose.model('booking', bookingSchema, 'booking')
+  booked_by: String,
+  cost: mongoose.Types.Decimal128,
+  
+  customer_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Customer' }],
+  customer_mood: String,
+  customer_status: String,
+  
+  location_id: String,
+  location_link: String,  
+  integration: String,
+  note: String,
+  slot: Number,
+  arrival_status: String,
+  
+  type: String,
+  
+  event_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
+  guest_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Customer' }],
+  site_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Site' },
+  add_on_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Site' }],
+  staff_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
+  workspace_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Workspace' },
+  created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
+  created_at: Date,  
+  created_from: String,
+  deleted: Boolean,
+  deleted_at: Date,
+  updated_at: Date
+}
+)
+const Booking = mongoose.model('Booking', bookingSchema)
 module.exports = Booking;
 
-/*
-arrival_status: String,
-assignee_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'staff'}],
-booked_by: String,
-booking_id: Number,
+// import mongoose from 'mongoose'
 
-discount_amount: Number,
-discount_applied: Boolean,
-discount_available: Boolean,
-discount_code: String,
-discount_id: { type: mongoose.Schema.Types.ObjectId, ref: 'discountcoupon'},
+// const bookingSchema = new mongoose.Schema({
+    
+//     workspace_id: { type: mongoose.Schema.Types.ObjectId, ref: 'workspace'}
+// })
 
-extra_time_is_blocking: Boolean,
-gift_card_amount: Number,
-gift_card_applied: Boolean,
-gift_card_available: Boolean,
-gift_card_id: { type: mongoose.Schema.Types.ObjectId, ref: 'giftcard'},
-guest_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'customer'}],
-integration: String,
-is_approved: Boolean,
-is_asked_for_review: Boolean,
-is_cancelled: Boolean,
-is_failed: Boolean,
-is_multi_service: Boolean,
-is_offer_applied: Boolean,
-is_offer_available: Boolean,
-is_paid: Boolean,
+// const Booking = mongoose.model('booking', bookingSchema, 'booking')
+// module.exports = Booking;
 
-offer_amount: Number,
-order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'order'},
-payment_mode_id: Number,
-platform: String,
-price_type: String,
-progress: [{ type: mongoose.Schema.Types.ObjectId, ref: 'bookingprogress'}],
-reference: String,
-resource_id: Number,
-room_id: String,
+// /*******
+//  add_on_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'addon'}],
+// amount_paid: Number,
+// appointment_booking_time: Date,
+// appointment_end_time: Date,
+// appointment_start_time: Date,
+// appointment_time_before_reschedule: [Date],
 
- */
+// cost: Number,
+// created_at: Date,
+// created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'staff'},
+// created_from: String,
+// customer_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'customer'}],
+// customer_mood: String,
+// customer_status: String,
+// deleted: Boolean,
+// deleted_at: Date,
+
+// duration_minutes: Number,
+// event_id: Event,
+
+// is_recurring: Boolean,
+// is_rescheduled: Boolean,
+// is_review_given: Boolean,
+// location_link: String,
+// location_setting_id: { type: mongoose.Schema.Types.ObjectId, ref: 'locationsetting'},
+// note: String,
+
+// site_id: { type: mongoose.Schema.Types.ObjectId, ref: 'site'},
+// slot: Number,
+// staff_id: { type: mongoose.Schema.Types.ObjectId, ref: 'staff'},
+// timezone: String,
+// transfered_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'staff'}],
+// type: String,
+// updated_at: Date,
+// /*
+// arrival_status: String,
+// assignee_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'staff'}],
+// booked_by: String,
+// booking_id: Number,
+
+// discount_amount: Number,
+// discount_applied: Boolean,
+// discount_available: Boolean,
+// discount_code: String,
+// discount_id: { type: mongoose.Schema.Types.ObjectId, ref: 'discountcoupon'},
+
+// extra_time_is_blocking: Boolean,
+// gift_card_amount: Number,
+// gift_card_applied: Boolean,
+// gift_card_available: Boolean,
+// gift_card_id: { type: mongoose.Schema.Types.ObjectId, ref: 'giftcard'},
+// guest_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'customer'}],
+// integration: String,
+// is_approved: Boolean,
+// is_asked_for_review: Boolean,
+// is_cancelled: Boolean,
+// is_failed: Boolean,
+// is_multi_service: Boolean,
+// is_offer_applied: Boolean,
+// is_offer_available: Boolean,
+// is_paid: Boolean,
+
+// offer_amount: Number,
+// order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'order'},
+// payment_mode_id: Number,
+// platform: String,
+// price_type: String,
+// progress: [{ type: mongoose.Schema.Types.ObjectId, ref: 'bookingprogress'}],
+// reference: String,
+// resource_id: Number,
+// room_id: String,
+
+//  */
--- schema/booking.js
+++ schema/booking.js
@@ -1,90 +1,181 @@
 import { gql } from 'apollo-server-express';
-
 export default gql`
 scalar Date
 type Booking {
   _id: ID
-    add_on_ids: [Addon]
-    amount_paid: Int
-    appointment_booking_time: String
-    appointment_end_time: String
-    appointment_start_time: String
-    appointment_time_before_reschedule: [String]
+  appointment_booking_time: String,
+  appointment_end_time: String,
+  appointment_start_time: String,
+  is_recurring: Boolean,
+  timezone: String,
     
-    cost: Int
-    created_at: String
-    created_by: Staff
-    created_from: String
-    customer_ids: [Customer]
-    customer_mood: String
-    customer_status: String
-    deleted: Boolean
-    deleted_at: String
-    
-    duration_minutes: Int
-    event_id: Event
-    
-    is_recurring: Boolean
-    is_rescheduled: Boolean
-    is_review_given: Boolean
-    location_link: String
-    location_setting_id: Locationsetting
-    note: String
-    
-    site_id: Site
-    slot: Int
-    staff_id: Staff
-    timezone: String
-    transfered_id: [Staff]
-    type: String
-    updated_at: String
-    workspace_id: Workspace
+  booked_by: String,
+  cost: Float,
+  
+  customer_ids: [Customer],
+  customer_mood: String,
+  customer_status: String,
+  location_id: String,
+  location_link: String,  
+  integration: String,
+  note: String,
+  slot: Int,
+  arrival_status: String,
+  
+  type: String,
+  
+  event_id: Event,
+  guest_ids: [Customer],
+  site_id: Site,
+  add_on_ids: [Site],
+  staff_id: Staff,
+  workspace_id: Workspace,
+  created_by: Staff,
+  created_at: String,  
+  created_from: String,
+  deleted: Boolean,
+  deleted_at: String,
+  updated_at: String
 }
-
+input bookingInput {
+  appointment_booking_time: String,
+  appointment_end_time: String,
+  appointment_start_time: String,
+  is_recurring: Boolean,
+  timezone: String,
+    
+  booked_by: String,
+  cost: Float,
+  
+  customer_ids: [ID],
+  customer_mood: String,
+  customer_status: String,
+  
+  location_id: String,
+  location_link: String,  
+  integration: String,
+  note: String,
+  slot: Int,
+  arrival_status: String,
+  
+  type: String,
+  
+  event_id: ID,
+  guest_ids: [ID],
+  site_id: ID,
+  add_on_ids: [ID],
+  staff_id: ID,
+  workspace_id: ID,
+  created_by: ID,
+  created_at: String,  
+  created_from: String,
+  deleted: Boolean,
+  deleted_at: String,
+  updated_at: String
+}
 extend type Query {
-  getBooking(workspace_id: ID, site_id: ID): [Booking]
+  getBooking: [Booking]
 }
-
 extend type Mutation {
-  addBooking(input: bookingInput): Booking  
+  addBooking(input: bookingInput): Booking
+  updateBooking(bookingID: ID!, input: bookingInput): Booking
+  deleteBooking(bookingID: ID!): Booking
 }
 `
 
-/*
-arrival_status: String
-assignee_id: [Staff]
-booked_by: String
-booking_id: Int
 
-discount_amount: Int
-discount_applied: Boolean
-discount_available: Boolean
-discount_code: String
-discount_id: Discountcoupon
+// import { gql } from 'apollo-server-express';
 
-extra_time_is_blocking: Boolean
-gift_card_amount: Int
-gift_card_applied: Boolean
-gift_card_available: Boolean
-gift_card_id: Giftcard
-guest_ids: [Customer]
-integration: String
-is_approved: Boolean
-is_asked_for_review: Boolean
-is_cancelled: Boolean
-is_failed: Boolean
-is_multi_service: Boolean
-is_offer_applied: Boolean
-is_offer_available: Boolean
-is_paid: Boolean
+// export default gql`
+// scalar Date
+// type Booking {
+//   _id: ID
+//   workspace_id: Workspace
+    
+// }
 
-offer_amount: Int
-order_id: Order
-payment_mode_id: Int
-platform: String
-price_type: String
-progress: [BookingProgress]
-reference: String
-resource_id: Int
-room_id: String
-*/
+// extend type Query {
+//   getBooking(workspace_id: ID, site_id: ID): [Booking]
+// }
+
+// extend type Mutation {
+//   addBooking(input: bookingInput): Booking  
+// }
+// `
+
+// /*******
+//  add_on_ids: [Addon]
+//     amount_paid: Int
+//     appointment_booking_time: String
+//     appointment_end_time: String
+//     appointment_start_time: String
+//     appointment_time_before_reschedule: [String]
+    
+//     cost: Int
+//     created_at: String
+//     created_by: Staff
+//     created_from: String
+//     customer_ids: [Customer]
+//     customer_mood: String
+//     customer_status: String
+//     deleted: Boolean
+//     deleted_at: String
+    
+//     duration_minutes: Int
+//     event_id: Event
+    
+//     is_recurring: Boolean
+//     is_rescheduled: Boolean
+//     is_review_given: Boolean
+//     location_link: String
+//     location_setting_id: Locationsetting
+//     note: String
+    
+//     site_id: Site
+//     slot: Int
+//     staff_id: Staff
+//     timezone: String
+//     transfered_id: [Staff]
+//     type: String
+//     updated_at: String
+//     workspace_id: Workspace
+//  */
+
+// /*
+// arrival_status: String
+// assignee_id: [Staff]
+// booked_by: String
+// booking_id: Int
+
+// discount_amount: Int
+// discount_applied: Boolean
+// discount_available: Boolean
+// discount_code: String
+// discount_id: Discountcoupon
+
+// extra_time_is_blocking: Boolean
+// gift_card_amount: Int
+// gift_card_applied: Boolean
+// gift_card_available: Boolean
+// gift_card_id: Giftcard
+// guest_ids: [Customer]
+// integration: String
+// is_approved: Boolean
+// is_asked_for_review: Boolean
+// is_cancelled: Boolean
+// is_failed: Boolean
+// is_multi_service: Boolean
+// is_offer_applied: Boolean
+// is_offer_available: Boolean
+// is_paid: Boolean
+
+// offer_amount: Int
+// order_id: Order
+// payment_mode_id: Int
+// platform: String
+// price_type: String
+// progress: [BookingProgress]
+// reference: String
+// resource_id: Int
+// room_id: String
+// */
